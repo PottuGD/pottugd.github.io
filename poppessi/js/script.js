24aff8f60f62a53ed84ec77163edc31de0cac14c
PottuGD <Sankiaho.pessi@gmail.com>
2022-11-06 16:50:41 +0200

    info to fix score

--- poppessi/js/script.js
+++ poppessi/js/script.js
@@ -89,7 +89,25 @@ function deleteCookie(cname) {
     document.cookie = cname + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 }
 
+
+/*
+
+copy these functions into the console if score is nan
+
 function fixScore(input){
     setCookie('fixScore', input, '9999999999999999999999');
     location.reload();
 }
+
+
+/////////////////////////////////////////////////////////////////////////
+
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+
+*/
